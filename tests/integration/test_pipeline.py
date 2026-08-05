d29566f97f0c3e4d4b679019b66ba6a0f2bdea1f
Kristhel Porras <kristhelporras@Kristhels-MacBook-Pro.local>
2026-08-04 20:48:50 -0600

    feat: preparar ejecución reproducible en Kabré

--- tests/integration/test_pipeline.py
+++ tests/integration/test_pipeline.py
@@ -1,9 +1,10 @@
 import pytest
+
+from renewable_atlas.composition import CompositionRoot
+from renewable_atlas.config import Settings
 from renewable_atlas.domain import GridPoint
 from renewable_atlas.infrastructure import FakeClimateDataSource
 from renewable_atlas.infrastructure.grid import SampleGridProvider
-from renewable_atlas.composition import CompositionRoot
-from renewable_atlas.config import Settings
 
 
 class TestAtlasPipeline:
@@ -19,7 +20,7 @@ class TestAtlasPipeline:
         pipeline = container.build_atlas_pipeline(use_fake=True)
         processor = container.build_sequential_processor()
 
-        grid_provider = SampleGridProvider(size=3, enable_sampling=True, sample_size=3)
+        grid_provider = SampleGridProvider(size=4, enable_sampling=True, sample_size=4)
         points = grid_provider.generate()
 
         assert len(points) > 0
